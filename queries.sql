b02e58e6d482d51e9694d91c6c267868763b47bf
Henoc Dz <roofdier.mx@gmail.com>
2012-12-28 23:16:37 -0600

    Query: Info Productos en Oferta

--- queries.sql
+++ queries.sql
@@ -6,4 +6,6 @@ SELECT * FROM producto WHERE idProducto NOT IN(SELECT idProducto FROM oferta WHE
 
 --
 --Información de los productos en oferta
+-- NOTA: Se debe especificar la fecha del "CLIENTE" 
+-- Formato fecha: 2012-12-31 08:00:00 
 SELECT oferta.idOferta, oferta.idProducto, (producto.precio-(producto.precio * oferta.descuento)) AS precio, oferta.descuento, producto.nombre,producto.url_img,producto.exitencia FROM producto NATURAL JOIN oferta WHERE (oferta.inicioOferta <= current_date()  AND oferta.finOferta > current_date())
