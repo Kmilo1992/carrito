de413f9d8930dfb9c90ee6c472384a113c59f56b
Henoc Dz <roofdier.mx@gmail.com>
2012-12-31 17:29:56 -0600

    Checando error de micarrito

--- application/views/micarrito.php
+++ application/views/micarrito.php
@@ -9,17 +9,21 @@
 		var tabla = "<thead><tr><th>Nombre</th><th>Precio</th></tr></thead>";
 		var arregloProductos = obtenerProductos();
 		for(i in arregloProductos){
+
 			var idProducto = arregloProductos[i];
 			$.ajax({
+				beforeSend: function(){console.log(idProducto)},
 				type: "POST",
-				url: "<?=base_url()?>index.php/productos/productoCarrito",
-				data: "id="+idProducto,
+				url: " <?=base_url()?>productos/productoCarrito",
+				data: {id:idProducto},
 				dataType: 'JSON',
 				success: function(data){
 					console.log(data);
 					tabla += formarColumna(data);
 					$("#tablaProductos").html(tabla);			
 				}
+			}).error(function(e,v){
+				console.log(e)
 			});
 		}
 		$('#borrar').on('click',function(){
