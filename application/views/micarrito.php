1e2dd0b9e4aa39bbccee1239f7aee013dbd897ed
Marcos Uriel Hernández Camacho <uriel.hdzc@gmail.com>
2013-01-01 16:02:49 -0600

    Integrar pago con paypal 1/2

--- application/views/micarrito.php
+++ application/views/micarrito.php
@@ -2,10 +2,13 @@
 <table class='table table-striped' id='tablaProductos'>
 
 </table>
-<button class='btn'>Pagar</button>
+<p><strong>Total: </strong><span id='total'></span> </p>
+<button class='btn' id='pagar'>Pagar</button>
 <button class='btn' id='borrar'>Borrar todo</button>
+
 <script>
 	$(document).on("ready",function(){
+		var costo_total= 0;
 		var tabla = "<thead><tr><th>Nombre</th><th>Precio</th></tr></thead>";
 		var arregloProductos = obtenerProductos();
 		for(i in arregloProductos){
@@ -17,6 +20,8 @@
 				dataType: 'JSON',
 				success: function(data){
 					console.log(data);
+					costo_total += parseFloat(data.precio);
+					$('#total').text(costo_total);
 					tabla += formarColumna(data);
 					$("#tablaProductos").html(tabla);			
 				}
@@ -26,9 +31,13 @@
 			vaciarProductos();
 			location.reload();
 		});
+		$('#pagar').on('click',function(){
+			window.location.href="<?= base_url() ?>ventas/crear/"+costo_total;
+		});
 	});
 		
 	function formarColumna(data){
+
 		var answer = "<tr>";
 		answer += "<td>"+data.nombre+"</td>";
 		answer += "<td>"+data.precio+"</td>";
